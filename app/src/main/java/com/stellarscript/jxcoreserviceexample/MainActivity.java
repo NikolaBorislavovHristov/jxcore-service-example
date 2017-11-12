f5fcf6435e048cfff9d50eba53cfe87be241740f
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-12 13:37:41 +0200

    Initial commit

---
+++ app/src/main/java/com/stellarscript/jxcoreserviceexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stellarscript.jxcoreserviceexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
