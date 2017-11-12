577e73cd1a71a83074df73c12e058cd0896a5bd5
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-12 18:28:45 +0200

    JXCore service started on main activity created

--- app/src/main/java/com/stellarscript/jxcoreserviceexample/MainActivity.java
+++ app/src/main/java/com/stellarscript/jxcoreserviceexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.stellarscript.jxcoreserviceexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.jxcore.node.JXCoreService;
 
 public final class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,9 @@ public final class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent startJXCoreIntent = new Intent(getApplicationContext(), JXCoreService.class);
+        startService(startJXCoreIntent);
     }
 
 }
